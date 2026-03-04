12692a3312a11a4ecc23b871ef41616218241c8f
Jonathan Dowland <jdowland@redhat.com>
2026-03-04 11:36:48 +0000

    Experimental support for generating rpms.in.yaml
    Assuming the contents of the file is (mostly) defined at the image
    descriptor key "konflux"."rpms.in.yaml", collate, unique and sort
    all the packages installed for the container (across all modules);
    add it to the 'packages' key and write the result to
    target/image/rpms.in.yaml
    
    Currnetly: no schema validation for presence of "rpms.in.yaml" key
    (current schema is too relaxed, accepts anything); we overwrite any
    existing packages key, might be nicer to merge.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- cekit/generator/konflux.py
+++ cekit/generator/konflux.py
@@ -1,6 +1,9 @@
 import logging
+import os
+import yaml
 LOGGER = logging.getLogger("cekit")
 
+from cekit.template_helper import TemplateHelper
 from cekit.generator.base import Generator
 
 class KonfluxGenerator(Generator):
@@ -14,3 +17,24 @@ class KonfluxGenerator(Generator):
         for image in self.images:
             for artifact in image.all_artifacts:
                 raise NotImplementedError("Artifacts handling is not implemented")
+
+    def generate(self) -> None:
+        super(KonfluxGenerator, self).generate()
+        self._render_rpm_lockfile()
+
+    def _render_rpm_lockfile(self) -> None:
+        y = self.image.get("konflux",{}).get("rpms.in.yaml", {})
+        if not y:
+            return
+
+        y['packages'] = self._populate_packages()
+
+        f = os.path.join(self.target, "image", "rpms.in.yaml")
+        with open(f, "w") as fh:
+            yaml.safe_dump(y, fh, default_flow_style=False)
+
+    def _populate_packages(self) -> None:
+        th = TemplateHelper(self._module_registry)
+        pkgs = list(set(th.packages_to_install(self.image)))
+        pkgs.sort()
+        return pkgs
